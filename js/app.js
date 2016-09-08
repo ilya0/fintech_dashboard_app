26b73baa780bc480cf58e2366c0bb7d8f081ed75
ilya osovets <iosovets@gmail.com>
2016-09-08 00:32:59 -0400

    apis added

--- js/app.js
+++ js/app.js
@@ -1,6 +1,66 @@
  console.log("connected js");
 
- var testarray = {Math.floor(Math.random()*100),test11,Math.floor(Math.random()*100),test12,Math.floor(Math.random()*100),test13};
+ var testarray = [
+  Math.floor(Math.random()*100),
+  "test11",
+  Math.floor(Math.random()*100),
+  "test12",
+  Math.floor(Math.random()*100),
+  "test13"];
+
+
+
+//copied code from the pubnub hello world
+function publish() {
+
+    pubnub = new PubNub({
+        publishKey : 'demo',
+        subscribeKey : 'demo'
+    })
+
+    function publishSampleMessage() {
+        console.log("Since we're publishing on subscribe connectEvent, we're sure we'll receive the following publish.");
+        var publishConfig = {
+            channel : "hello_world",
+            message : "Hello from PubNub Docs!"
+        }
+        pubnub.publish(publishConfig, function(status, response) {
+            console.log(status, response);
+        })
+    }
+
+    pubnub.addListener({
+        status: function(statusEvent) {
+            if (statusEvent.category === "PNConnectedCategory") {
+                publishSampleMessage();
+            }
+        },
+        message: function(message) {
+            console.log("New Message!!", message);
+        },
+        presence: function(presenceEvent) {
+            // handle presence
+        }
+    })
+    console.log("Subscribing..");
+    pubnub.subscribe({
+        channels: ['hello_world']
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  //test array
 
 // var angles = [ 30, 10, 45, 35, 60, 38, 75, 67 ];
